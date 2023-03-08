56545cb1dbde942c3c874521ae679279c9aa622c
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-03-08 20:27:26 +0100

    small changes to header

--- src/Components/SectionFour/SectionFour.js
+++ src/Components/SectionFour/SectionFour.js
@@ -6,40 +6,41 @@ import dash from '../../assets/dash.png'
 import playStore from '../../assets/playStore.svg'
 import appleStore from '../../assets/appleStore.svg'
 import Applink from '../AppLinks/Applink'
+import { Slide } from 'react-awesome-reveal'
 
 const SectionFour = () => {
   return (
     <section className='sectionFour'>
-        <aside>
-            <h2>
-                Its not just banking,
-                Its a way of life
-            </h2>
-                <br />
-                <br />
-            <p>
-                Join us now and get started.
-                Faster, easier and secure transactions
-                at your finger tips.
-            </p>
-                <br />
-                <br />
-            <small>YOU'VE NEVER HAD IT THIS GOOD!</small>
-                <br />
-                <br />
+            <aside>
+                <h2>
+                    Its not just banking,
+                    Its a way of life
+                </h2>
+                    <br />
+                    <br />
+                <p>
+                    Join us now and get started.
+                    Faster, easier and secure transactions
+                    at your finger tips.
+                </p>
+                    <br />
+                    <br />
+                <small>YOU'VE NEVER HAD IT THIS GOOD!</small>
+                    <br />
+                    <br />
 
-                <Applink />
+                    <Applink />
 
-                <br />
-                <br />
+                    <br />
+                    <br />
 
-                <Link to='/' className='next'> Learn More <i class="fa-solid fa-arrow-right"></i></Link>
+                    <Link to='/' className='next'> Learn More <i class="fa-solid fa-arrow-right"></i></Link>
 
-        </aside>
+            </aside>
 
-        <img src={relaxed} alt="" id='relax'/>
+            <img src={relaxed} alt="" id='relax'/>
 
-        <img src={dash} alt="" id='dash'/>
+            <img src={dash} alt="" id='dash'/>
     </section>
   )
 }
